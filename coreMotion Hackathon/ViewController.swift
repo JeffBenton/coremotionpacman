5e1bbb5d2d281dc1a058b684f31b23402a17a4c3
Jeff Benton <jeff@frogrock.net>
2015-05-04 15:48:13 -0700

    full project

--- coreMotion Hackathon/ViewController.swift
+++ coreMotion Hackathon/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import CoreMotion
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var currentlyPlaying: UILabel!
+    var playing = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let motion: CMMotionManager = CMMotionManager()
+     var waka = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("PacmanWakaWaka", ofType: "mp3")!)
+    var audioWaka = AVAudioPlayer()
+    var opening = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("PacmanOpeningSong", ofType: "mp3")!)
+    var audioOpening = AVAudioPlayer()
+    var closing = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("PacmanDies", ofType: "mp3")!)
+    var audioClosing = AVAudioPlayer()
+    var eating = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("PacmanEatingGhost", ofType: "mp3")!)
+    var audioEating = AVAudioPlayer()
+
+    
+    @IBAction func startButton(sender: UIButton) {
+        if !playing {
+            audioOpening.play()
+            self.currentlyPlaying.text = "Herro"
+            motion.accelerometerUpdateInterval = 0.1
+            
+            motion.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue(), withHandler: { (accelerometerData: CMAccelerometerData!, error: NSError!) -> Void in self.outputAcclerationData(accelerometerData.acceleration)
+                if error != nil {
+                    println("\(error)")
+                }
+            })
+            playing = true
+        }
     }
 
+    @IBAction func stopButton(sender: UIButton) {
+        if playing {
+            audioClosing.play()
+            self.currentlyPlaying.text = "Play again"
+            motion.stopAccelerometerUpdates()
+            playing = false
+        }
+    }
+    
+    override func viewDidLoad() {
+        audioWaka = AVAudioPlayer(contentsOfURL: waka, error: nil)
+        audioWaka.prepareToPlay()
+        audioOpening = AVAudioPlayer(contentsOfURL: opening, error: nil)
+        audioOpening.prepareToPlay()
+        audioClosing = AVAudioPlayer(contentsOfURL: closing, error: nil)
+        audioClosing.prepareToPlay()
+        audioEating = AVAudioPlayer(contentsOfURL: eating, error: nil)
+        audioEating.prepareToPlay()
+        super.viewDidLoad()
+    }
+
+    
+    func outputAcclerationData(acceleration: CMAcceleration) {
+        if acceleration.x > 2 || acceleration.x < -2 {
+            audioWaka.play()
+            self.currentlyPlaying.text = "Waka waka"
+        }
+        if acceleration.y > 2 || acceleration.y < -2 {
+            audioWaka.play()
+            self.currentlyPlaying.text = "Waka waka"
+        }
+        if acceleration.z > 2 || acceleration.z < -2 {
+            audioEating.play()
+            self.currentlyPlaying.text = "Eating a ghost"
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
